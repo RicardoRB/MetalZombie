d25e402e40fcb1d42c8eabbb9396cdf2d4e63883
Richy501 <soulfear91@gmail.com>
2013-01-18 21:45:48 +0100

    First commit of the hame :)

---
+++ include/Main.h
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#ifndef MAIN_H
+#define MAIN_H
+
+class Main
+{
+    public:
+        Main();
+        virtual ~Main();
+    protected:
+    private:
+};
+
+#endif // MAIN_H
---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+#include "../include/Main.h"
+
+using namespace std;
+using namespace sf;
+
+int main(){
+    cout << "First commit of the game :)" << endl;
+    return 0;
+}
